4b1f2c3b974fe61238da49b496e08f98404edd6c
Guilherme Seleri <110575996+guilhermesealves@users.noreply.github.com>
2025-11-26 06:40:24 -0300

    Comit para css

--- projeto-cep/src/App.jsx
+++ projeto-cep/src/App.jsx
@@ -1,79 +1,103 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import './App.css';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
 
 function App() {
-
-  const {register, handleSubmit, setValue, setFocus} = useForm();
+  const { register, handleSubmit, setValue, setFocus } = useForm();
+  const [showFields, setShowFields] = useState(false);
 
   const onSubmit = (e) => {
     console.log(e);
-  }
+  };
 
   const checkCEP = (e) => {
-    const cep = e.target.value.replace(/\D/g, '');
+    const cep = e.target.value.replace(/\D/g, "");
     if (cep.length !== 8) return;
-    fetch(`https://viacep.com.br/ws/${cep}/json/`).then(res => res.json()).then(data => {
-       if (data.erro) {
-        alert("CEP não encontrado!");
-        return; 
-      }
-      console.log(data);
-      setValue('address', data.logradouro);
-      setValue('neighborhood', data.bairro);
-      setValue('city', data.localidade);
-      setValue('uf', data.uf);
-      setFocus('addressNumber');
-    });
-  }
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.erro) {
+          alert("CEP não encontrado!");
+          return;
+        }
+
+        setShowFields(true);
+
+        setValue("address", data.logradouro);
+        setValue("neighborhood", data.bairro);
+        setValue("city", data.localidade);
+        setValue("uf", data.uf);
+        setFocus("addressNumber");
+      });
+  };
+  const handleCancel = () => {
+    setShowFields(false);
+    setValue("address", "");
+    setValue("addressNumber", "");
+    setValue("neighborhood", "");
+    setValue("city", "");
+    setValue("uf", "");
+    setFocus("cep");
+  };
+
 
   return (
     <div className="FormAll">
-  <form onSubmit={handleSubmit(onSubmit)}>
-    <h1>Add Event</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Consultar CEP</h1>
 
-    <div className="input-group">
-      <label>
-        CEP *
-        <input type="text" {...register("cep")} onBlur={checkCEP} />
-      </label>
+        {/* CAMPO CEP */}
+        <div className="input-group">
+          <label>
+            CEP *
+            <input type="text" {...register("cep")} onBlur={checkCEP} />
+          </label>
+        </div>
 
-      <label>
-        Rua
-        <input type="text" {...register("address")} />
-      </label>
+        {/* CAMPOS QUE APARECEM DEPOIS */}
+        <div className={`hidden-fields ${showFields ? "show" : ""}`}>
+          <div className="input-group">
+            <label>
+              Rua
+              <input type="text" {...register("address")} />
+            </label>
+
+            <label>
+              Número
+              <input type="text" {...register("addressNumber")} />
+            </label>
+          </div>
+
+          <div className="input-group">
+            <label>
+              Bairro
+              <input type="text" {...register("neighborhood")} />
+            </label>
+
+            <label>
+              Cidade
+              <input type="text" {...register("city")} />
+            </label>
+          </div>
+
+          <div className="input-group">
+            <label>
+              Estado
+              <input type="text" {...register("uf")} />
+            </label>
+          </div>
+
+          <div className="buttons">
+            <button type="button" onClick={handleCancel} className="cancel">
+              Cancelar
+            </button>
+
+            <button onhan className="send">Enviar</button>
+          </div>
+        </div>
+      </form>
     </div>
-
-    <div className="input-group">
-      <label>
-        Número
-        <input type="text" {...register("addressNumber")} />
-      </label>
-
-      <label>
-        Bairro
-        <input type="text" {...register("neighborhood")} />
-      </label>
-    </div>
-
-    <div className="input-group">
-      <label>
-        Cidade
-        <input type="text" {...register("city")} />
-      </label>
-
-      <label>
-        Estado
-        <input type="text" {...register("uf")} />
-      </label>
-    </div>
-
-    <div className="buttons">
-      <button type="button" className="cancel">Cancelar</button>
-      <button type="submit" className="send">Enviar</button>
-    </div>
-  </form>
-</div>
 
   );
 }
